61f4d814bb65242ae24eec680746a738225f156a
Dasbakon <felipe.lira.oliveira@gmail.com>
2019-08-12 22:06:40 -0300

    +Adicionado pop up se usuário não colocar nome ou idade na tela inicial

--- uLiL.ME/ViewController.swift
+++ uLiL.ME/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func enterButton(_ sender: UIButton) {
+        if nameTextField.text?.isEmpty == true {
+            
+            let alertController = UIAlertController(title: "ERRO", message: "Nome está em branco", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else if ageTextField.text?.isEmpty == true {
+            
+            let alertController = UIAlertController(title: "ERRO", message: "Idade está em branco", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            let interestsViewController = self.storyboard?.instantiateViewController(withIdentifier: "CollectionViewControllerID") as! CollectionViewController
+            
+            interestsViewController.name = nameTextField.text
+            interestsViewController.age = Int(ageTextField.text!)
+            
+        }
+        
+    }
 
 }
 
